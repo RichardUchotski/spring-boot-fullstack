2db9eccf207ba6f49932163cd419df771fc4dd9e
Richard Smith-Uchotski <richard_smith_uchotski@hotmail.co.uk>
2024-10-03 09:53:39 +0100

    CD Workflow with ping pong controller for test

---
+++ backend/src/main/java/org/amigoscodersuapi/PingPongController.java
@@ -0,0 +1,15 @@
+package org.amigoscodersuapi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingPongController {
+    record PingPong(String message) {
+    }
+
+    @GetMapping
+    public PingPong getPingPong() {
+        return new PingPong("PingPong");
+    }
+}
