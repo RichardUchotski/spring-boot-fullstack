72913b2d0740bcff23f8ff609d92689be8860041
Richard Smith-Uchotski <richard_smith_uchotski@hotmail.co.uk>
2024-10-03 11:10:43 +0100

    Adding pong or ping or removing pingpong form controller for test

--- backend/src/main/java/org/amigoscodersuapi/PingPongController.java
+++ backend/src/main/java/org/amigoscodersuapi/PingPongController.java
@@ -10,6 +10,6 @@ public class PingPongController {
 
     @GetMapping
     public PingPong getPingPong() {
-        return new PingPong("PongPongPingPongPing");
+        return new PingPong("PongPongPingPongPongPing");
     }
 }
