eb15c9d34d7f7b429d8f10acc56ca22b4bac51ef
Richard Smith-Uchotski <richard_smith_uchotski@hotmail.co.uk>
2024-10-15 13:06:41 +0100

    Updated workflow with Slack webhook and modified PingPongController

--- backend/src/main/java/org/amigoscodersuapi/PingPongController.java
+++ backend/src/main/java/org/amigoscodersuapi/PingPongController.java
@@ -10,6 +10,6 @@ public class PingPongController {
 
     @GetMapping
     public PingPong getPingPong() {
-        return new PingPong("PongPongPangPangPingPingPongPingPangPangPongPing");
+        return new PingPong("PongPongPangPangPingPangPingPongPingPangPangPongPing");
     }
 }
