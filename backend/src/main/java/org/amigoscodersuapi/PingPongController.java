46390ea743c469d07ae31f883ae09ce6ad747113
Richard Smith-Uchotski <richard_smith_uchotski@hotmail.co.uk>
2024-10-15 13:14:30 +0100

    Adding a

--- backend/src/main/java/org/amigoscodersuapi/PingPongController.java
+++ backend/src/main/java/org/amigoscodersuapi/PingPongController.java
@@ -10,6 +10,6 @@ public class PingPongController {
 
     @GetMapping
     public PingPong getPingPong() {
-        return new PingPong("PongPongPangPangPingPangAAAPingPongPingPangPangPongPing");
+        return new PingPong("PongPongPangPangPingPangAAAAPingPongPingPangPangPongPing");
     }
 }
