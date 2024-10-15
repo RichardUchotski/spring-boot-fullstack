3a8b597e8e345e8765ca998599e0b09ac011bdab
Richard Smith-Uchotski <richard_smith_uchotski@hotmail.co.uk>
2024-10-15 12:03:38 +0100

    Changing the file so it changes the workflow

--- backend/src/main/java/org/amigoscodersuapi/PingPongController.java
+++ backend/src/main/java/org/amigoscodersuapi/PingPongController.java
@@ -10,6 +10,6 @@ public class PingPongController {
 
     @GetMapping
     public PingPong getPingPong() {
-        return new PingPong("PongPongPingPongPangPangPongPing");
+        return new PingPong("PongPongPingPongPingPangPangPongPing");
     }
 }
