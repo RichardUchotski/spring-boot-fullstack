5996eddb135fff2c4bd1cb0908a6866e520bc6d8
Richard Smith-Uchotski <richard_smith_uchotski@hotmail.co.uk>
2024-10-15 16:21:29 +0100

    Using the secret

--- backend/src/main/java/org/amigoscodersuapi/PingPongController.java
+++ backend/src/main/java/org/amigoscodersuapi/PingPongController.java
@@ -10,6 +10,6 @@ public class PingPongController {
 
     @GetMapping
     public PingPong getPingPong() {
-        return new PingPong("PongPongPangPangPingPangAAAAAPingPongPingPangPangPongPing");
+        return new PingPong("Pong");
     }
 }
