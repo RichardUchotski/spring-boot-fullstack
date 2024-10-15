c2af334f29dd0d89287ceb44d337f8fdfb815267
Richard Smith-Uchotski <richard_smith_uchotski@hotmail.co.uk>
2024-10-15 14:39:18 +0100

    Adding correct slack hook

--- backend/src/main/java/org/amigoscodersuapi/PingPongController.java
+++ backend/src/main/java/org/amigoscodersuapi/PingPongController.java
@@ -10,6 +10,6 @@ public class PingPongController {
 
     @GetMapping
     public PingPong getPingPong() {
-        return new PingPong("PongPongPangPangPingPangAAAAPingPongPingPangPangPongPing");
+        return new PingPong("PongPongPangPangPingPangAAAAAPingPongPingPangPangPongPing");
     }
 }
