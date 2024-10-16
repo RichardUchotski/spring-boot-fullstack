83179f3153bb607e42c282f61d927f5b11d203de
Richard Smith-Uchotski <richard_smith_uchotski@hotmail.co.uk>
2024-10-16 14:35:34 +0100

    Adding slack messages to the workflow

--- backend/src/main/java/org/amigoscodersuapi/PingPongController.java
+++ backend/src/main/java/org/amigoscodersuapi/PingPongController.java
@@ -3,13 +3,16 @@ package org.amigoscodersuapi;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("/ping")
 public class PingPongController {
+
+    private static int COUNTER  = 0;
+
     record PingPong(String message) {
     }
 
     @GetMapping
     public PingPong getPingPong() {
-        return new PingPong("PongPingPangPingPang");
+        return new PingPong("Pong " + ++COUNTER);
     }
 }
